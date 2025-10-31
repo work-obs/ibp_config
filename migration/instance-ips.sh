3ace6326017ac56641c3da4a7a6c05d9720d8e2e
Bennie Kahler-Venter <bennie@obsidian.co.za>
2025-10-31 12:47:21 +0200

    updates

---
+++ migration/instance-ips.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# EC2 instance private IPs
+# Generated from AWS CLI queries based on instance names
+
+INSTANCE_IPS=(
+    "10.0.1.100"
+    "10.0.1.101"
+    "10.0.2.100"
+    # ... etc
+)
